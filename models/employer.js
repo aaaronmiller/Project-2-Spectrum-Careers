6be9d1adc216aaa1182432bdce2917d62706c630
Aaron Miller <aaaronmiller@gmail.com>
2019-07-19 23:15:39 -0700

    user profile

--- models/employer.js
+++ models/employer.js
@@ -1,12 +1,16 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Employer = sequelize.define("Employer", {
-    username: DataTypes.STRING,
-    pw: DataTypes.STRING,
+    // username: DataTypes.STRING,
+    // pw: DataTypes.STRING,
+    //defined by google
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     photo: DataTypes.STRING,
+    //defined by profile page
+    phone: DataTypes.STRING,
     bio: DataTypes.STRING,
+    location: DataTypes.STRING
   });
   return Employer;
 }
